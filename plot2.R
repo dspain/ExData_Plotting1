6712127a936f7b98a146a43528c5aabaea4244ce
dspain <dmspain@yahoo.com>
2014-05-10 19:10:23 -0700

    create plot2

---
+++ plot2.R
@@ -0,0 +1,24 @@
+## plot2 generates a plot identical to "Plot 2" in the readme file
+#   dataFile = name & location of file containing the dataset.  
+plot2 <- function(dataFile) {
+  data <- read.csv(dataFile,
+                   header=TRUE,sep=";",
+                   colClasses=c("character","character",rep("numeric",7)), 
+                   na.strings="?")
+  data$DateTime <- strptime(paste(data$Date, data$Time, sep=" "), 
+                            format="%d/%m/%Y %H:%M:%S")
+  data$Date <- as.Date(data$Date, 
+                       format="%d/%m/%Y")
+  dataSubset <- subset(data, 
+                       data$Date %in% as.Date(c('2007-02-01','2007-02-02'), 
+                                              format="%Y-%m-%d"))
+  plot(dataSubset$DateTime,
+       dataSubset$Global_active_power,
+       type="l",
+       xlab="",
+       ylab="Global Active Power (kilowatts)")
+  dev.copy(png, file='plot2.png')
+  dev.off()
+}
+
+plot2("../household_power_consumption.txt")
